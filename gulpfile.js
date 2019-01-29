8bbdd44c0ae4d59fd369d99bfe78918383caa8e0
Brandon Ore <y2ktheory@gmail.com>
2019-01-29 12:39:35 -0700

    removed extra files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp'),
+      browserSync = require('browser-sync').create(),
+      sass = require('gulp-sass');
+
+// compile sass
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+})
+
+// watch and serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['*.html']).on('change', browserSync.reload);
+})
+
+// default
+gulp.task('default', ['serve']);
