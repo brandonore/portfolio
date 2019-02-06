89278bde6f02c2e1ef5524bd6e120467973f56f2
Brandon Ore <y2ktheory@gmail.com>
2019-02-05 18:10:06 -0700

    small tweaks. added screens

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,64 @@ $("a").on('click', function(event) {
   //gallery 
 $('.img-gallery').on('click', function() {
     let id = $(this).attr('id');
+    
+    switch (id) {
+        case 'i1':
+            $.fancybox.open([
+                {
+                    src  : 'https://i.imgur.com/tQF8Mno.png',
+                    opts : {
+                        caption : 'Blank preview'
+                    }
+                },
+                {
+                    src  : 'https://i.imgur.com/uAu9vnS.png',
+                    opts : {
+                        caption : 'Example invoice'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/eEAU48k.png',
+                    opts : {
+                        caption : 'Date picker'
+                    }
+                }
+            ], {
+                loop : true,
+                thumbs : {
+                    autoStart : true
+                }
+            });
+            break;
+
+        case 'i2': 
+            $.fancybox.open([
+                {
+                    src  : 'https://i.imgur.com/WkwONkK.mp4',
+                    opts : {
+                        caption : 'App in action'
+                    }
+                },
+                {
+                    src  : 'https://i.imgur.com/uC64mir.png',
+                    opts : {
+                        caption : 'Home view'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/Stzdtxj.png',
+                    opts : {
+                        caption : 'Satoshi to USD conversion'
+                    }
+                }
+            ], {
+                loop : true,
+                thumbs : {
+                    autoStart : true
+                }
+            });
+            break;
+    }
 
     // $.fancybox.open([
     //     {
