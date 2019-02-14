15cc27f4af156a33529cf6713712e288e5544b42
Brandon Ore <y2ktheory@gmail.com>
2019-02-13 17:30:11 -0700

    styled contact form. added upload file input

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+$('input[type="file"]').change(function(e){
+    var fileName = e.target.files[0].name;
+    document.getElementById("label").innerHTML = `${fileName} is selected`;
+});
+
 //smooth scrolling for browsers that dont support css property
 $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -199,5 +204,98 @@ $('.img-gallery').on('click', function() {
                 break;
     }
   })
+				
+    jQuery.validator.addMethod("filetype", function(value, element) {
+        var types = ['jpeg', 'jpg', 'png', 'gif', 'doc', 'docx', 'xls', 'xlsx', 'pdf', 'txt', 'rtf', 'zip', 'rar'],	
+            ext = value.replace(/.*[.]/, '').toLowerCase();
+    
+        if (types.indexOf(ext) !== -1) {
+            return true;
+        }
+        return false;
+        },
+      "Upload supported files only."
+    );
+        
 
-  
+    $('#form').validate({
+        ignore: ".ignore",
+            rules:{
+                "name":{
+                    required:true,
+                    maxlength:40		
+                },
+
+                "email":{
+                    required:true,
+                    email:true,
+                    maxlength:100		
+                },
+                
+                "userfile[]":{
+                    required:true,
+                    filetype:true
+                },
+
+                "message":{
+                    required:true
+                },
+                
+                "hiddenRecaptcha":{		
+                    required: function () {
+                    if (grecaptcha.getResponse() == '') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }},
+                
+
+            messages:{
+                "name":{
+                    required:"Username is required."		
+                },
+
+                "email":{
+                    required:"Email is required.",		
+                    email:"Please enter a valid email address."		
+                },
+                
+                "subject":{
+                    required:"Subject is required.",		
+                },
+                
+                "userfile[]":{
+                    required:"Please upload your file."		
+                },
+                
+                "message":{
+                    required:"Please enter your message."		
+                },
+                
+                "hiddenRecaptcha":{
+                    required:"Captcha is required."		
+                }},
+                
+                
+                errorLabelContainer: '#errors',
+                   wrapper:'li',
+                
+            submitHandler: function(form){
+            $(form).ajaxSubmit({
+            url: 'mail/mailplusattachment.php',
+            type: 'post',
+            data: $('form').serialize(),
+            success: function (data) {
+                $('.xyz').fadeIn("slow").html('Your Message has been sent!');
+                setTimeout(function() {
+                $('.xyz').fadeOut("slow");
+                $('#form')[0].reset();
+                grecaptcha.reset();
+                }, 5000 );
+            }
+        }); 
+     }
+   })
+
