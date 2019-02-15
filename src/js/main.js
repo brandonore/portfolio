2419d04b900b4996ed909afd4adeab2de1f9abe5
Brandon Ore <y2ktheory@gmail.com>
2019-02-14 19:13:47 -0700

    added google captcha. cleaned up css

--- src/js/main.js
+++ src/js/main.js
@@ -1,170 +1,49 @@
-$('input[type="file"]').change(function(e){
-    var fileName = e.target.files[0].name;
-    document.getElementById("label").innerHTML = `${fileName} is selected`;
-});
+jQuery(document).ready(function($) {
 
-//smooth scrolling for browsers that dont support css property
-$("a").on('click', function(event) {
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-      let hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }
-  });
-
-  //gallery 
-$('.img-gallery').on('click', function() {
-    let id = $(this).attr('id');
+    // display file attachment name
+    $('input[type="file"]').change(function(e){
+        var fileName = e.target.files[0].name;
+        document.getElementById("label").innerHTML = `${fileName} is selected`;
+    });
     
-    switch (id) {
-        case 'i1':
-            $.fancybox.open([
-                {
-                    src  : 'https://i.imgur.com/tQF8Mno.png',
-                    opts : {
-                        caption : 'Blank preview'
-                    }
-                },
-                {
-                    src  : 'https://i.imgur.com/uAu9vnS.png',
-                    opts : {
-                        caption : 'Example invoice'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/eEAU48k.png',
-                    opts : {
-                        caption : 'Date picker'
-                    }
-                }
-            ], {
-                loop : true,
-                thumbs : {
-                    autoStart : true
-                }
-            });
-            break;
-
-        case 'i2': 
-            $.fancybox.open([
-                {
-                    src  : 'https://i.imgur.com/WkwONkK.mp4',
-                    opts : {
-                        caption : 'App in action'
-                    }
-                },
-                {
-                    src  : 'https://i.imgur.com/uC64mir.png',
-                    opts : {
-                        caption : 'Home view'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/Stzdtxj.png',
-                    opts : {
-                        caption : 'Satoshi to USD conversion'
-                    }
-                }
-            ], {
-                loop : true,
-                thumbs : {
-                    autoStart : true
-                }
-            });
-            break;
-
-        case 'i3': 
-            $.fancybox.open([
-                {
-                    src  : 'https://i.imgur.com/W1coGFo.png',
-                    opts : {
-                        caption : 'Get info from a specific anime'
-                    }
-                },
-                {
-                    src  : 'https://i.imgur.com/CR55qsJ.png',
-                    opts : {
-                        caption : 'Report user example'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/lZ9cf2v.png',
-                    opts : {
-                        caption : 'Random doggo example'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/wGlQJjq.png',
-                    opts : {
-                        caption : 'Check online players for a private game server'
-                    }
-                }
-            ], {
-                loop : true,
-                thumbs : {
-                    autoStart : true
-                }
-            });
-            break;
-
-        case 'i4': 
-            $.fancybox.open([
-                {
-                    src  : 'https://i.imgur.com/7eWNKVD.png',
-                    opts : {
-                        caption : 'Home view'
-                    }
-                },
-                {
-                    src  : 'https://i.imgur.com/e3khBEd.png',
-                    opts : {
-                        caption : 'Filter by keyword'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/QwLb1Es.png',
-                    opts : {
-                        caption : 'Edit/Remove expense'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/uw10O1b.png',
-                    opts : {
-                        caption : 'Date picker'
-                    }
-                },
-                {
-                    src : 'https://i.imgur.com/y0uD3vu.png',
-                    opts : {
-                        caption : 'Login'
-                    }
-                }
-            ], {
-                loop : true,
-                thumbs : {
-                    autoStart : true
-                }
-            });
-            break;
-
-            case 'i5': 
+    //smooth scrolling for browsers that dont support css property
+    $("a").click(function(event) {
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+          let hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        }
+      });
+    
+      // fancybox gallery 
+    $('.img-gallery').click(function() {
+        let id = $(this).attr('id');
+        
+        switch (id) {
+            case 'i1':
                 $.fancybox.open([
                     {
-                        src  : 'https://i.imgur.com/hWKeZvL.png',
+                        src  : 'https://i.imgur.com/tQF8Mno.png',
                         opts : {
-                            caption : 'Main view'
+                            caption : 'Blank preview'
                         }
                     },
                     {
-                        src  : 'https://i.imgur.com/ZthGjck.png',
+                        src  : 'https://i.imgur.com/uAu9vnS.png',
                         opts : {
-                            caption : 'Sample config file'
+                            caption : 'Example invoice'
+                        }
+                    },
+                    {
+                        src : 'https://i.imgur.com/eEAU48k.png',
+                        opts : {
+                            caption : 'Date picker'
                         }
                     }
                 ], {
@@ -174,25 +53,25 @@ $('.img-gallery').on('click', function() {
                     }
                 });
                 break;
-
-            case 'i6': 
+    
+            case 'i2': 
                 $.fancybox.open([
                     {
-                        src  : 'https://i.imgur.com/BOOEWUh.png',
+                        src  : 'https://i.imgur.com/WkwONkK.mp4',
                         opts : {
-                            caption : 'Example data exported to Excel sheet'
+                            caption : 'App in action'
                         }
                     },
                     {
-                        src  : 'https://i.imgur.com/B2GKB9j.png',
+                        src  : 'https://i.imgur.com/uC64mir.png',
                         opts : {
-                            caption : 'CMD status text'
+                            caption : 'Home view'
                         }
                     },
                     {
-                        src  : 'https://i.imgur.com/XwiZGUR.png',
+                        src : 'https://i.imgur.com/Stzdtxj.png',
                         opts : {
-                            caption : 'Success message'
+                            caption : 'Satoshi to USD conversion'
                         }
                     }
                 ], {
@@ -202,10 +81,134 @@ $('.img-gallery').on('click', function() {
                     }
                 });
                 break;
-    }
-  })
-				
-    jQuery.validator.addMethod("filetype", function(value, element) {
+    
+            case 'i3': 
+                $.fancybox.open([
+                    {
+                        src  : 'https://i.imgur.com/W1coGFo.png',
+                        opts : {
+                            caption : 'Get info from a specific anime'
+                        }
+                    },
+                    {
+                        src  : 'https://i.imgur.com/CR55qsJ.png',
+                        opts : {
+                            caption : 'Report user example'
+                        }
+                    },
+                    {
+                        src : 'https://i.imgur.com/lZ9cf2v.png',
+                        opts : {
+                            caption : 'Random doggo example'
+                        }
+                    },
+                    {
+                        src : 'https://i.imgur.com/wGlQJjq.png',
+                        opts : {
+                            caption : 'Check online players for a private game server'
+                        }
+                    }
+                ], {
+                    loop : true,
+                    thumbs : {
+                        autoStart : true
+                    }
+                });
+                break;
+    
+            case 'i4': 
+                $.fancybox.open([
+                    {
+                        src  : 'https://i.imgur.com/7eWNKVD.png',
+                        opts : {
+                            caption : 'Home view'
+                        }
+                    },
+                    {
+                        src  : 'https://i.imgur.com/e3khBEd.png',
+                        opts : {
+                            caption : 'Filter by keyword'
+                        }
+                    },
+                    {
+                        src : 'https://i.imgur.com/QwLb1Es.png',
+                        opts : {
+                            caption : 'Edit/Remove expense'
+                        }
+                    },
+                    {
+                        src : 'https://i.imgur.com/uw10O1b.png',
+                        opts : {
+                            caption : 'Date picker'
+                        }
+                    },
+                    {
+                        src : 'https://i.imgur.com/y0uD3vu.png',
+                        opts : {
+                            caption : 'Login'
+                        }
+                    }
+                ], {
+                    loop : true,
+                    thumbs : {
+                        autoStart : true
+                    }
+                });
+                break;
+    
+                case 'i5': 
+                    $.fancybox.open([
+                        {
+                            src  : 'https://i.imgur.com/hWKeZvL.png',
+                            opts : {
+                                caption : 'Main view'
+                            }
+                        },
+                        {
+                            src  : 'https://i.imgur.com/ZthGjck.png',
+                            opts : {
+                                caption : 'Sample config file'
+                            }
+                        }
+                    ], {
+                        loop : true,
+                        thumbs : {
+                            autoStart : true
+                        }
+                    });
+                    break;
+    
+                case 'i6': 
+                    $.fancybox.open([
+                        {
+                            src  : 'https://i.imgur.com/BOOEWUh.png',
+                            opts : {
+                                caption : 'Example data exported to Excel sheet'
+                            }
+                        },
+                        {
+                            src  : 'https://i.imgur.com/B2GKB9j.png',
+                            opts : {
+                                caption : 'CMD status text'
+                            }
+                        },
+                        {
+                            src  : 'https://i.imgur.com/XwiZGUR.png',
+                            opts : {
+                                caption : 'Success message'
+                            }
+                        }
+                    ], {
+                        loop : true,
+                        thumbs : {
+                            autoStart : true
+                        }
+                    });
+                    break;
+        }
+      })
+                    
+    $.validator.addMethod("filetype", function(value, element) {
         var types = ['jpeg', 'jpg', 'png', 'gif', 'doc', 'docx', 'xls', 'xlsx', 'pdf', 'txt', 'rtf', 'zip', 'rar'],	
             ext = value.replace(/.*[.]/, '').toLowerCase();
     
@@ -214,10 +217,10 @@ $('.img-gallery').on('click', function() {
         }
         return false;
         },
-      "Upload supported files only."
+        "Upload supported files only."
     );
-        
-
+            
+    
     $('#form').validate({
         ignore: ".ignore",
             rules:{
@@ -225,22 +228,14 @@ $('.img-gallery').on('click', function() {
                     required:true,
                     maxlength:40		
                 },
-
                 "email":{
                     required:true,
                     email:true,
                     maxlength:100		
                 },
-                
-                "userfile[]":{
-                    required:true,
-                    filetype:true
-                },
-
                 "message":{
                     required:true
                 },
-                
                 "hiddenRecaptcha":{		
                     required: function () {
                     if (grecaptcha.getResponse() == '') {
@@ -250,38 +245,25 @@ $('.img-gallery').on('click', function() {
                     }
                 }
             }},
-                
-
             messages:{
                 "name":{
-                    required:"Username is required."		
+                    required:"Name is required."		
                 },
-
                 "email":{
                     required:"Email is required.",		
                     email:"Please enter a valid email address."		
                 },
-                
                 "subject":{
                     required:"Subject is required.",		
                 },
-                
-                "userfile[]":{
-                    required:"Please upload your file."		
-                },
-                
                 "message":{
                     required:"Please enter your message."		
                 },
-                
                 "hiddenRecaptcha":{
                     required:"Captcha is required."		
                 }},
-                
-                
                 errorLabelContainer: '#errors',
-                   wrapper:'li',
-                
+                    wrapper:'li',
             submitHandler: function(form){
             $(form).ajaxSubmit({
             url: 'mail/mailplusattachment.php',
@@ -296,6 +278,8 @@ $('.img-gallery').on('click', function() {
                 }, 5000 );
             }
         }); 
-     }
-   })
+        }
+    })
+});
+
 
