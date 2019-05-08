f8d70eae960edb1b9796adfd156dfa04788d63da
Brandon Ore <y2ktheory@gmail.com>
2019-05-08 13:50:18 -0400

    added horizontal bar chart for skills. css tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,27 @@
 $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
+    // $('#page-content-wrapper').toggleClass('toggled');
 });
 
-const ctx = document.getElementById('menu-chart').getContext("2d")
+const ctx = document.getElementById('menu-chart').getContext("2d");
+const ctx1 = document.getElementById('skills-chart').getContext("2d");
 
-const gradientStroke = ctx.createLinearGradient(200, 0, 100, 0);
-gradientStroke.addColorStop(0, '#74ebd5');
-gradientStroke.addColorStop(1, '#9face6');
+const gs = ctx.createLinearGradient(200, 0, 100, 0);
+gs.addColorStop(0, '#74ebd5');
+gs.addColorStop(1, '#9face6');
 
-const gradientFill = ctx.createLinearGradient(200, 0, 100, 0);
-gradientFill.addColorStop(0, "rgba(116, 235, 213, 0.6)");
-gradientFill.addColorStop(1, "rgba(159, 172, 230, 0.6)");
+const gf = ctx.createLinearGradient(200, 0, 100, 0);
+gf.addColorStop(0, "rgba(116, 235, 213, 0.6)");
+gf.addColorStop(1, "rgba(159, 172, 230, 0.6)");
+
+const gs1 = ctx.createLinearGradient(0, 500, 200, 0);
+gs1.addColorStop(0, '#21d4fd');
+gs1.addColorStop(1, '#b721ff');
+
+const gf1 = ctx.createLinearGradient(0, 500, 200, 0);
+gf1.addColorStop(0, "rgba(33, 212, 253, 0.6)");
+gf1.addColorStop(1, "rgba(183, 33, 255, 0.6)");
 
 let myChart = new Chart(ctx, {
     type: 'line',
@@ -19,17 +29,17 @@ let myChart = new Chart(ctx, {
         labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL"],
         datasets: [{
             label: "Commits by Month",
-            borderColor: gradientStroke,
-            pointBorderColor: gradientStroke,
-            pointBackgroundColor: gradientStroke,
-           pointHoverBackgroundColor: gradientStroke,
-            pointHoverBorderColor: gradientStroke,
+            borderColor: gs,
+            pointBorderColor: gs,
+            pointBackgroundColor: gs,
+            pointHoverBackgroundColor: gs,
+            pointHoverBorderColor: gs,
             pointBorderWidth: 10,
             pointHoverRadius: 10,
             pointHoverBorderWidth: 1,
             pointRadius: .5,
             fill: true,
-            backgroundColor: gradientFill,
+            backgroundColor: gf,
             borderWidth: 4,
             data: [2, 5, 12, 9, 9, 5, 12]
         }]
@@ -61,6 +71,32 @@ let myChart = new Chart(ctx, {
                     padding: 20,
                     fontColor: "rgba(0,0,0,0.5)",
                     fontStyle: "bold"
+                }
+            }]
+        }
+    }
+});
+
+let myChart1 = new Chart(ctx1, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['HTML5', 'CSS3', 'JavaScript', 'Vue', 'Node', 'Electron'],
+        datasets: [{
+            label: 'Value in %',
+            borderColor: gs1,
+            fill: true,
+            backgroundColor: [gf1, gf1, gf1, gf1, gf1, gf1],
+            borderWidth: 4,
+            data: [70, 75, 60, 35, 50, 80]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 100,
+                    stepSize: 10
                 }
             }]
         }
