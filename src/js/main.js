b7c3e67cdc5ad7943f2bdbdffa101fe9609fafdb
Brandon Ore <y2ktheory@gmail.com>
2019-05-30 11:52:24 -0600

    added gallery for project images. small tweaks

--- src/js/main.js
+++ src/js/main.js
@@ -193,3 +193,162 @@ $('.fa-info-circle').on('click', function() {
             break;
     }
 });
+
+ // fancybox gallery 
+$('.img-gallery').click(function() {
+    let id = $(this).attr('id');
+
+    switch (id) {
+        case 'i2':
+            $.fancybox.open([
+                {
+                    src  : 'https://i.imgur.com/tQF8Mno.png',
+                    opts : {
+                        caption : 'Blank preview'
+                    }
+                },
+                {
+                    src  : 'https://i.imgur.com/uAu9vnS.png',
+                    opts : {
+                        caption : 'Example invoice'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/eEAU48k.png',
+                    opts : {
+                        caption : 'Date picker'
+                    }
+                }
+            ], {
+                loop : true,
+                thumbs : {
+                    autoStart : true
+                }
+            });
+            break;
+
+        case 'i3': 
+            $.fancybox.open([
+                {
+                    src  : 'https://i.imgur.com/WkwONkK.mp4',
+                    opts : {
+                        caption : 'App in action'
+                    }
+                },
+                {
+                    src  : 'https://i.imgur.com/uC64mir.png',
+                    opts : {
+                        caption : 'Home view'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/Stzdtxj.png',
+                    opts : {
+                        caption : 'Satoshi to USD conversion'
+                    }
+                }
+            ], {
+                loop : true,
+                thumbs : {
+                    autoStart : true
+                }
+            });
+            break;
+
+        case 'i5': 
+            $.fancybox.open([
+                {
+                    src  : 'https://i.imgur.com/W1coGFo.png',
+                    opts : {
+                        caption : 'Get info from a specific anime'
+                    }
+                },
+                {
+                    src  : 'https://i.imgur.com/CR55qsJ.png',
+                    opts : {
+                        caption : 'Report user example'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/lZ9cf2v.png',
+                    opts : {
+                        caption : 'Random doggo example'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/wGlQJjq.png',
+                    opts : {
+                        caption : 'Check online players for a private game server'
+                    }
+                }
+            ], {
+                loop : true,
+                thumbs : {
+                    autoStart : true
+                }
+            });
+            break;
+
+        case 'i4': 
+            $.fancybox.open([
+                {
+                    src  : 'https://i.imgur.com/7eWNKVD.png',
+                    opts : {
+                        caption : 'Home view'
+                    }
+                },
+                {
+                    src  : 'https://i.imgur.com/e3khBEd.png',
+                    opts : {
+                        caption : 'Filter by keyword'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/QwLb1Es.png',
+                    opts : {
+                        caption : 'Edit/Remove expense'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/uw10O1b.png',
+                    opts : {
+                        caption : 'Date picker'
+                    }
+                },
+                {
+                    src : 'https://i.imgur.com/y0uD3vu.png',
+                    opts : {
+                        caption : 'Login'
+                    }
+                }
+            ], {
+                loop : true,
+                thumbs : {
+                    autoStart : true
+                }
+            });
+            break;
+
+            case 'i6': 
+                $.fancybox.open([
+                    {
+                        src  : 'https://i.imgur.com/hWKeZvL.png',
+                        opts : {
+                            caption : 'Main view'
+                        }
+                    },
+                    {
+                        src  : 'https://i.imgur.com/ZthGjck.png',
+                        opts : {
+                            caption : 'Sample config file'
+                        }
+                    }
+                ], {
+                    loop : true,
+                    thumbs : {
+                        autoStart : true
+                    }
+                });
+                break;
+    }
+  });
